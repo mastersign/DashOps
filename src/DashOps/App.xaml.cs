b3a6a17f12634b0a4f9f1c0baf24c985d1b0c75f
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2018-04-23 14:13:25 +0200

    added support for default project file (fixes #1)

--- src/DashOps/App.xaml.cs
+++ src/DashOps/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,30 +20,68 @@ namespace Mastersign.DashOps
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            if (e.Args.Length < 1)
+            var projectFile = default(string);
+            if (e.Args.Length == 1)
             {
-                MessageBox.Show("No project file given. Pass a path to a project file as command line argument.");
-                Shutdown(1);
+                if (File.Exists(e.Args[0]))
+                    projectFile = e.Args[0];
+                else
+                {
+                    MessageBox.Show(
+                        $"The project file '{e.Args[0]}' could not be found.",
+                        "Loading DashOps Project File",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    Shutdown(1);
+                    return;
+                }
             }
-            if (e.Args.Length > 1)
+            else
             {
-                MessageBox.Show("Only one command line argument is allowed.");
-                Shutdown(1);
+                var name1 = Path.Combine(Environment.CurrentDirectory, "dashops.yaml");
+                var name2 = Path.Combine(Environment.CurrentDirectory, "dashops.yml");
+                if (File.Exists(name1))
+                    projectFile = name1;
+                else if (File.Exists(name2))
+                    projectFile = name2;
+                else
+                {
+                    MessageBox.Show(
+                        $"No project file given as command line argument and no default project file in the current working directory: {name1}",
+                        "Loading DashOps Project File",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                    Shutdown(1);
+                    return;
+                }
             }
             try
             {
-                ProjectLoader = new ProjectLoader(e.Args[0], Dispatch);
+                ProjectLoader = new ProjectLoader(projectFile, Dispatch);
             }
             catch (Exception exc)
             {
-                var msg = exc.Message;
+                var msg = string.Empty;
+#if DEBUG
+                msg += exc.ToString();
+#else
+                msg += exc.Message;
+#endif
                 while (exc.InnerException != null)
                 {
                     exc = exc.InnerException;
+#if DEBUG
+                    msg += Environment.NewLine + exc.ToString();
+#else
                     msg += Environment.NewLine + exc.Message;
+#endif
                 }
-                MessageBox.Show(msg);
+                MessageBox.Show(msg,
+                    "Loading DashOps Project File",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
                 Shutdown(1);
+                return;
             }
             Executor = new Executor();
         }
